6d88f38e285032e5c1b743a350bcacb1b52f619a
n4ryn <vinaykumar.cs19@aryacollege.in>
2025-06-19 08:57:05 +0530

    feat: add login and signup apis

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,29 @@
 import express from "express";
-const app = express();
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+dotenv.config();
 
+// Routes
+import authRouter from "./routes/auth.routes";
+
+const app = express();
 const port = 9000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.listen(port, () => {
-  return console.log(`Server is listening at http://localhost:${port}`);
-});
+app.use("/", authRouter);
+
+connectDB()
+  .then(() => {
+    console.log("Database connected");
+
+    app.listen(port, () => {
+      return console.log(`Server is listening at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to database", error);
+  });
---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,104 @@
+import { Request, Response, Router } from "express";
+import bcrypt from "bcrypt";
+
+// Models
+import User from "../models/user.model";
+
+// Validations
+import {
+  loginValidation,
+  signupValidation,
+} from "../validations/auth.validation";
+
+const router = Router();
+
+// Signup
+router.post(
+  "/signup",
+  signupValidation,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { name, email, password } = req.body;
+
+      // Check if user already exists with same email
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        res.status(400).json({ message: "User already exists" });
+        return;
+      }
+
+      // Hash password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create new user
+      const newUser = new User({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      // Generate JWT token
+      const token = await newUser.generateJWT();
+
+      // Save user to database
+      await newUser.save();
+
+      res
+        .cookie("token", token, {
+          expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
+          httpOnly: true,
+        })
+        .status(201)
+        .json({
+          message: "Signup successful",
+          data: { token, user: newUser },
+        });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+// Login
+router.post(
+  "/login",
+  loginValidation,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { email, password } = req.body;
+
+      // Check if user exists
+      const existingUser = await User.findOne({ email });
+      if (!existingUser) {
+        res.status(404).json({ message: "User doesn't exists" });
+        return;
+      }
+
+      // Validate password
+      const isPasswordValid = await existingUser.validatePassword(password);
+      if (!isPasswordValid) {
+        res.status(401).json({ message: "Invalid password" });
+      }
+
+      // Generate JWT token
+      const token = await existingUser.generateJWT();
+
+      res
+        .cookie("token", token, {
+          expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        })
+        .status(200)
+        .send({
+          message: "Login successful",
+          data: { token, user: existingUser },
+        });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+export default router;
